a7c289ec263b8997eb00b00f1f80ac19e5923da1
mathemartins <mathegeniuse@gmail.com>
2023-11-29 11:40:27 +0100

    initial commit config

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.getElementById('phoneForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+// });
+
+window.onload = function() {
+    if (window.jQuery) {  
+        console.log("Fuck! I worked ")
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
